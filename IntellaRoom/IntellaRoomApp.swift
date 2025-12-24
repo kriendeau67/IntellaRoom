9da1cb051654b8c662ebf085ff00c6cfc4f16ba7
Kenneth Riendeau <kriendeau67@gmail.com>
2025-12-24 09:28:41 -0500

    Initial Commit

---
+++ IntellaRoom/IntellaRoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntellaRoomApp.swift
+//  IntellaRoom
+//
+//  Created by Kenneth Riendeau on 12/24/25.
+//
+
+import SwiftUI
+
+@main
+struct IntellaRoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
